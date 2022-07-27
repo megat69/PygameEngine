9af63de83e511e14362aeca1bcb6128f31f92a3c
megat69 <assassincraft218@gmail.com>
2022-07-27 20:33:21 +0200

    Added exception for invalid color

--- src/PygameEngine/tiles.py
+++ src/PygameEngine/tiles.py
@@ -8,5 +8,9 @@ class Tile:
 		:param color: Color of the tile, in (r, g, b) format.
 		:param enabled: Whether the tile should be displayed. True by default.
 		"""
+		# Checking that the color is a valid RGB color
+		if any(color[i] > 255 or color[i] < 0 for i in range(len(color))):
+			raise Exception(f"Invalid color on tile creation : {color}")
+
 		self.color = color
 		self.enabled = enabled
